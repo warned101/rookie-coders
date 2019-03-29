ff1ea3526ecb0a81e2d25a65619a7f76b0a884ae
warned101 <astitvagupta123@gmail.com>
2019-03-29 17:20:53 +0530

    Hello World in C++

---
+++ c++_programming/hello_world.cpp
@@ -0,0 +1,14 @@
+// Simple C++ program to display "Hello World" 
+  
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function 
+int main() { 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+} 
